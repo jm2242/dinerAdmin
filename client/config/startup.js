dc059542f7fbc61612f2a09c516b3e15acbe61f5
Jonathan Mares <jmares93@gmail.com>
2016-07-10 14:05:21 -0400

    some text changes

---
+++ client/config/startup.js
@@ -0,0 +1,3 @@
+/**
+ * Created by jonathanmares on 7/9/16.
+ */
--- client/views/submittedOrders/submittedOrders.js
+++ client/views/submittedOrders/submittedOrders.js
@@ -2,17 +2,38 @@
  * Created by jonathanmares on 7/9/16.
  */
 
+
+Template.submittedOrders.onRendered(function () {
+    const numberOfOrders = submittedOrders.find({}).count();
+    Session.set("numberOfOrders", numberOfOrders);
+});
+
 Template.submittedOrders.helpers({
+    // get the submitted orders. reactive function
     getSubmittedOrders: function () {
-      return submittedOrders.find({});
+
+        return submittedOrders.find({});
     },
 
     settings: function () {
         const tableSettingsObject = {
             fields: [{key: 'customerName', label: 'Customer Name'},
-                {key: 'customerLocation',label: 'Customer Location'}]
+                {key: 'customerLocation',label: 'Customer Address'},
+                {key:'meal', label: 'Meal'},
+                {key:'orderMethod',label:'Order Method'},
+                {key:'delivered',label:'Delivered'}],
+            showFilter: false
         };
         return tableSettingsObject;
+    },
+
+    numberOfOrders: function () {
+        const numberOfOrders = submittedOrders.find({}).count();
+        if (numberOfOrders >  Session.get("numberOfOrders")) {
+            sAlert.info('You have received a new order!',{position:'top',effect:'jelly'});
+        }
+        return numberOfOrders;
+
     }
 
 });
